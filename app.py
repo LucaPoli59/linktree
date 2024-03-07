72528502e7dced65cae585ea83f718871509a48f
Luca Poli <poli.luca.marconi@gmail.com>
2024-03-07 22:27:36 +0100

    added whitenoise

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
+from whitenoise import WhiteNoise
 from dash import html
 
 app = dash.Dash(__name__, use_pages=True,
